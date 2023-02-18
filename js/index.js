270d35a432f47788f60e32b33d0ba63adbfb0eed
timotheegarot <timotheegarot@hotmail.com>
2023-02-18 13:02:43 +0100

    Selection of languages added and js files moved to a js folder

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const fromText = document.querySelector(".from-text");
+const toText = document.querySelector(".to-text");
+const exchangeIcon = document.querySelector(".exchange");
+const selectTag = document.querySelector("select");
+const icons = document.querySelector(".row i");
+const translateBtn = document.querySelector("button");
+
+selectTag.forEach((tag, id) => {
+  for (let country_code in countries) {
+    let selected = id == 0 ? country_code == "en-GB" ? "selected" : "" : country_code == "de-DE" ? "selected" : "";
+    let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
+    tag.insertAdjacentHTML("beforeend", option);
+  }
+});
