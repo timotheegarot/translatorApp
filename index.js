9342bcc8c62e66201d5f9451a65ab066d6847a99
timotheegarot <timotheegarot@hotmail.com>
2023-02-18 12:41:14 +0100

    JS Constants created

---
+++ index.js
@@ -0,0 +1,6 @@
+const fromText = document.querySelector(".from-text");
+const toText = document.querySelector(".to-text");
+const exchangeIcon = document.querySelector(".exchange");
+const selectTag = document.querySelector(".select");
+const icons = document.querySelector(".row i");
+const translateBtn = document.querySelector(".button");
