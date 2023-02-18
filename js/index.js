404600534dc35f1b7bca84918a5fe424b1a6490f
timotheegarot <timotheegarot@hotmail.com>
2023-02-18 13:09:11 +0100

    Languages selection fixed

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 const fromText = document.querySelector(".from-text");
 const toText = document.querySelector(".to-text");
-const exchangeIcon = document.querySelector(".exchange");
-const selectTag = document.querySelector("select");
-const icons = document.querySelector(".row i");
+const exchageIcon = document.querySelector(".exchange");
+const selectTag = document.querySelectorAll("select");
+const icons = document.querySelectorAll(".row i");
 const translateBtn = document.querySelector("button");
 
 selectTag.forEach((tag, id) => {
